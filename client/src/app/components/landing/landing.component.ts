11494afe0c44854ccc8585b4942b460e807d6bc8
n-ab <32076158+n-ab@users.noreply.github.com>
2020-11-19 20:36:49 -0600

    landing page utilizes adobe fonts, removed photo/color correction services

--- client/src/app/components/landing/landing.component.ts
+++ client/src/app/components/landing/landing.component.ts
@@ -38,40 +38,6 @@ export class LandingComponent implements OnInit, AfterViewInit {
     this.color =    document.getElementById('color');
     this.foley =    document.getElementById('foley');
     this.panels.push(this.prod, this.post, this.photo, this.color, this.foley);
-    this.prod.addEventListener('click', () => {
-      this.sortPanels(this.prod);
-      this.nav.navigateTo('prod');
-    });
-    this.post.addEventListener('click', () => {
-      this.sortPanels(this.post);
-    });
-    this.photo.addEventListener('click', () => {
-      this.sortPanels(this.photo);
-    });
-    this.color.addEventListener('click', () => {
-      this.sortPanels(this.color);
-    });
-    this.foley.addEventListener('click', () => {
-      this.sortPanels(this.foley);
-    });
-  }
-
-  sortPanels(save: HTMLElement): void {
-    console.log('this.panels = ', this.panels);
-    this.toggleBackButton();
-    const spliceVal = this.panels.indexOf(save, 0);
-    this.panels.splice(spliceVal, 1);
-    this.panels.forEach(panel => {
-      this.panelFadeOut();
-      panel.classList.add('fadeAway');
-    });
-    setTimeout(() => {
-      this.panels.forEach(panel => {
-        this.panelFadeOut();
-        this.landing.classList.add('lower-height');
-        // panel.classList.add('display-none');
-      });
-    }, 400);
   }
 
   toggleBackButton(): void {
@@ -79,20 +45,6 @@ export class LandingComponent implements OnInit, AfterViewInit {
     if    (this.showBackButton === true) { this.showBackButton = false; }
     else  { this.showBackButton = true; }
     console.log('what button toggle IS = ', this.showBackButton);
-  }
-
-  panelFadeOut(): void {
-
-  }
-
-  showAllPanels(): void {
-    this.toggleBackButton();
-    this.landing.classList.remove('lower-height');
-    this.panels.forEach(panel => {
-      panel.classList.remove('fadeAway');
-    });
-    this.panels = [];
-    this.panels.push(this.prod, this.post, this.photo, this.color, this.foley);
   }
 
 }
